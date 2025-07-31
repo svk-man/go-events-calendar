9272b00e44b595423fe0d57218704ea2658f84f7
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2025-08-01 00:12:07 +0300

    feat: add NewEvent func

--- events/events.go
+++ events/events.go
@@ -1,8 +1,26 @@
 package events
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"github.com/araddon/dateparse"
+)
 
 type Event struct {
 	Title   string
 	StartAt time.Time
+}
+
+func NewEvent(title string, dateStr string) (Event, error) {
+	t, err := dateparse.ParseAny(dateStr)
+
+	if err != nil {
+		return Event{}, errors.New("неверный формат даты")
+	}
+
+	return Event{
+		Title:   title,
+		StartAt: t,
+	}, nil
 }
--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"go-events-calendar/calendar"
 	"go-events-calendar/events"
-	"fmt"
-	"time"
 )
 
 func main() {
-	e := events.Event{
-		Title:   "Встреча",
-		StartAt: time.Now(),
+	e, err := events.NewEvent("Встреча", "2024-07-15 09:30")
+	if err != nil {
+		fmt.Println("Ошибка создания события:", err)
+		return
 	}
+
 	calendar.AddEvent("event1", e)
 	fmt.Println("Календарь обновлён")
 	fmt.Scanln()
